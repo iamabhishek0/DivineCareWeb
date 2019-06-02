705c229d4eb0b81db4a8a8d8dee2a838b794eed3
Siddharth Vaish <sidvaish97@gmail.com>
2019-06-02 15:51:17 +0530

    videos from db to html

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class video(models.Model):
+    videoName=models.CharField(max_length=200)
+    videoDesc=models.CharField(max_length=200)
+    url=models.CharField(max_length=200)
+    videoId=models.CharField(max_length=200)
+    publishedAt=models.IntegerField()
--- home/views.py
+++ home/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pyrebase
+
+firebaseConfig = {
+    'apiKey': "AIzaSyB4LuASCKc3QxocENU6T16hRGQu6gTILT4",
+    'authDomain': "divinecareapp-2ec91.firebaseapp.com",
+    'databaseURL': "https://divinecareapp-2ec91.firebaseio.com",
+    'projectId': "divinecareapp-2ec91",
+    'storageBucket': "divinecareapp-2ec91.appspot.com",
+    'messagingSenderId': "142556288828",
+    'appId': "1:142556288828:web:9614f8f60904601f"
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
 
 # Create your views here.
 
 def sample(request):
-
     return render(request, "index.html")
 
 
 def home(request):
-    return render(request, "youtube_videos.html")
+    db = firebase.database()
+    video = db.child("videosnew").get().val()
+    videolink = []
+    for k, v in video.items():
+        videolink.append("https://www.youtube.com/embed/" + video[k]['videoId'])
+
+    users = db.child("users").get()
+    return render(request, "youtube_videos.html", {'videolink': videolink})
